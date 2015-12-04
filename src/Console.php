01f91a1a3ed4ff424666b83f14c47bf757487ce6
Jacob Emerick <jpemeric@gmail.com>
2015-12-04 06:42:09 -0700

    Adds cleaner Console without GLOBALS dependency

--- src/Console.php
+++ src/Console.php
@@ -1,92 +1,109 @@
 <?php
 
-/* - - - - - - - - - - - - - - - - - - - - -
-
- Title : PHP Quick Profiler Console Class
- Author : Created by Ryan Campbell
- URL : http://particletree.com/features/php-quick-profiler/
-
- Last Updated : April 22, 2009
-
- Description : This class serves as a wrapper around a global
- php variable, debugger_logs, that we have created.
-
-- - - - - - - - - - - - - - - - - - - - - */
+/*****************************************
+ * Title : PHP Quick Profiler Console Class
+ * Author : Created by Ryan Campbell
+ * URL : http://particletree.com/features/php-quick-profiler/
+ * Description : This class serves as a wrapper to hold onto
+ *  various messages and profiling data
+ *****************************************/
 
 namespace Particletree\Pqp;
+use Exception;
 
-class Console {
-	
-	/*-----------------------------------
-	     LOG A VARIABLE TO CONSOLE
-	------------------------------------*/
-	
-	public static function log($data) {
-		$logItem = array(
-			"data" => $data,
-			"type" => 'log'
-		);
-		$GLOBALS['debugger_logs']['console'][] = $logItem;
-		$GLOBALS['debugger_logs']['logCount'] += 1;
-	}
-	
-	/*---------------------------------------------------
-	     LOG MEMORY USAGE OF VARIABLE OR ENTIRE SCRIPT
-	-----------------------------------------------------*/
-	
-	public function logMemory($object = false, $name = 'PHP') {
-		$memory = memory_get_usage();
-		if($object) $memory = strlen(serialize($object));
-		$logItem = array(
-			"data" => $memory,
-			"type" => 'memory',
-			"name" => $name,
-			"dataType" => gettype($object)
-		);
-		$GLOBALS['debugger_logs']['console'][] = $logItem;
-		$GLOBALS['debugger_logs']['memoryCount'] += 1;
-	}
-	
-	/*-----------------------------------
-	     LOG A PHP EXCEPTION OBJECT
-	------------------------------------*/
-	
-	public function logError($exception, $message) {
-		$logItem = array(
-			"data" => $message,
-			"type" => 'error',
-			"file" => $exception->getFile(),
-			"line" => $exception->getLine()
-		);
-		$GLOBALS['debugger_logs']['console'][] = $logItem;
-		@$GLOBALS['debugger_logs']['errorCount'] += 1;
-	}
-	
-	/*------------------------------------
-	     POINT IN TIME SPEED SNAPSHOT
-	-------------------------------------*/
-	
-	public function logSpeed($name = 'Point in Time') {
-		$logItem = array(
-			"data" => PhpQuickProfiler::getMicroTime(),
-			"type" => 'speed',
-			"name" => $name
-		);
-		$GLOBALS['debugger_logs']['console'][] = $logItem;
-		$GLOBALS['debugger_logs']['speedCount'] += 1;
-	}
-	
-	/*-----------------------------------
-	     SET DEFAULTS & RETURN LOGS
-	------------------------------------*/
-	
-	public function getLogs() {
-		if(!$GLOBALS['debugger_logs']['memoryCount']) $GLOBALS['debugger_logs']['memoryCount'] = 0;
-		if(!$GLOBALS['debugger_logs']['logCount']) $GLOBALS['debugger_logs']['logCount'] = 0;
-		if(!$GLOBALS['debugger_logs']['speedCount']) $GLOBALS['debugger_logs']['speedCount'] = 0;
-		if(!$GLOBALS['debugger_logs']['errorCount']) $GLOBALS['debugger_logs']['errorCount'] = 0;
-		return $GLOBALS['debugger_logs'];
-	}
+class Console
+{
+
+    /** @var  array */
+    protected $log = array();
+
+    /** @var  array */
+    protected $memory = array();
+
+    /** @var  array */
+    protected $error = array();
+
+    /** @var  array */
+    protected $speed = array();
+
+    /**
+     * Logs data to the console
+     * Accepts any data type
+     *
+     * @param mixed $data
+     */
+    public function log($data)
+    {
+        array_push($this->logs, $data);
+    }
+
+    /**
+     * Logs memory usage of a variable
+     * If no parameter is passed in, logs current memory usage
+     *
+     * @param mixed $object
+     * @param string $name
+     */
+    public function logMemory($object = null, $name = '')
+    {
+        if (!is_null($object)) {
+            $memory = strlen(serialize($object));
+        } else {
+            $memory = memory_get_usage();
+            $name = 'PHP';
+        }
+
+        array_push($this->memory, array(
+            'data' => $memory,
+            'name' => $name,
+            'type' => gettype($object)
+        ));
+    }
+
+    /**
+     * Logs exception with optional message override
+     *
+     * @param Exception $exception
+     * @param string    $message
+     */
+    public function logError(Exception $exception, $message = '')
+    {
+        if (empty($message)) {
+            $message = $exception->getMessage();
+        }
+
+        array_push($this->error, array(
+            'data' => $message,
+            'file' => $exception->getFile(),
+            'line' => $exception->getLine(),
+        ));
+    }
+
+    /**
+     * Logs current time with optional message
+     *
+     * @param string $name
+     */
+    public function logSpeed($name = 'Point in Time')
+    {
+        array_push($this->speed, array(
+            'data' => microtime(true),
+            'name' => $name,
+        ));
+    }
+
+    /**
+     * Returns the collected logs
+     *
+     * @returns array
+     */
+    public function getLogs()
+    {
+        return array(
+            'log'    => $this->log,
+            'memory' => $this->memory,
+            'error'  => $this->error,
+            'speed'  => $this->speed
+        );
+    }
 }
-
-?>
