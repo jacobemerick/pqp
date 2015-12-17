db8ab4ca26185e4206f4449043d369f529b8a60d
Jacob Emerick <jpemeric@gmail.com>
2015-12-17 05:41:57 -0700

    Adds some in-house namespace hacks for built-in function testing

--- tests/unit/PhpQuickProfilerTest.php
+++ tests/unit/PhpQuickProfilerTest.php
@@ -4,14 +4,38 @@ namespace Particletree\Pqp;
 
 use PHPUnit_Framework_Testcase;
 
+// namespace hack on microtime functionality
+function microtime($get_as_float = false) {
+    if ($get_as_float) {
+      return 1450355136.5706;
+    }
+    return '1450355136 5706';
+}
+
+// namespace hack on included files functionality
+function get_included_files() {
+    return array(
+      'index.php',
+      'src/Class.php'
+    );
+}
+
+// namespace hack on filesize
+function filesize($filename) {
+    return 100;
+}
+
 class PhpQuickProfilerTest extends PHPUnit_Framework_TestCase
 {
 
     public function testConstruct()
     {
         $startTime = microtime(true);
-        $profiler = new PhpQuickProfiler($startTime);
 
+        $profiler = new PhpQuickProfiler();
+        $this->assertAttributeEquals($startTime, 'startTime', $profiler);
+
+        $profiler = new PhpQuickProfiler($startTime);
         $this->assertAttributeEquals($startTime, 'startTime', $profiler);
     }
 
@@ -31,6 +55,21 @@ class PhpQuickProfilerTest extends PHPUnit_Framework_TestCase
         $profiler->setDisplay($display);
 
         $this->assertAttributeSame($display, 'display', $profiler);
+    }
+
+    public function testGatherFileData()
+    {
+        $files = get_included_files();
+        $fileSize = filesize('');
+        $profiler = new PhpQuickProfiler();
+        $gatheredFileData = $profiler->gatherFileData();
+
+        foreach ($gatheredFileData as $fileData) {
+            $this->assertArrayHasKey('name', $fileData);
+            $this->assertContains($fileData['name'], $files);
+            $this->assertArrayHasKey('size', $fileData);
+            $this->assertEquals($fileData['size'], $fileSize);
+        }
     }
 
     public function testSetProfiledQueries()
